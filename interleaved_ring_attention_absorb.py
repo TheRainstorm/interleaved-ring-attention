aa78f55fddae820a2f9da9a056dd3432b5d2a08d
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2025-12-01 01:21:32 +0800

    test: ring attention + absorb pass

---
+++ interleaved_ring_attention_absorb.py
@@ -0,0 +1,151 @@
+import torch
+# from flash_mla import get_mla_metadata, flash_mla_with_kvcache
+from utils import *
+    
+def main():
+    torch.manual_seed(42)
+    
+    B, S_ori = 1, 26
+    cp_nranks = 4
+    
+    h_q, h_kv= 128, 1
+    kv_lora_rank = 512
+    v_head_dim = 128
+    qk_head_dim = 128
+    qk_rope_head_dim = 64
+    v_head_dim = 128
+    
+    assert B == 1, "This test only supports B=1"
+    
+    # 确保序列长度能被 rank 数整除
+    S = (S_ori + cp_nranks - 1) // cp_nranks * cp_nranks
+    S_local = S // cp_nranks
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    dtype = torch.float32
+    use_log2 = True
+    
+    # 定义统一的缩放因子
+    scale = (qk_head_dim + qk_rope_head_dim) ** -0.5
+    print(f"Using manual scale: {scale}")
+    
+    # print all parameters
+    print(f"B={B}, h_q={h_q}, h_kv={h_kv}, S_ori={S_ori} (Padded S={S}), "
+          f"qk_head_dim={qk_head_dim}, qk_rope_head_dim={qk_rope_head_dim}, "
+          f"kv_lora_rank={kv_lora_rank}, v_head_dim={v_head_dim}, CP={cp_nranks}")
+    print(f"Using device: {device}, dtype: {dtype}, use_log2: {use_log2}")
+    
+    q_nope_ori = torch.randn(B, S_ori, h_q, qk_head_dim, device=device, dtype=dtype)
+    q_pe_ori = torch.randn(B, S_ori, h_q, qk_rope_head_dim, device=device, dtype=dtype)
+    c_kv_ori = torch.randn(B, S_ori, kv_lora_rank, device=device, dtype=dtype)
+    k_pe_ori = torch.randn(B, S_ori, qk_rope_head_dim, device=device, dtype=dtype)
+    
+    wkv_b_q = torch.randn(h_q, kv_lora_rank, qk_head_dim, device=device, dtype=dtype)
+    wkv_b_o = torch.randn(h_q, kv_lora_rank, v_head_dim, device=device, dtype=dtype)
+    
+    # padding zero
+    q_nope = torch.zeros(B, S, h_q, qk_head_dim, device=device, dtype=dtype)
+    q_pe = torch.zeros(B, S, h_q, qk_rope_head_dim, device=device, dtype=dtype)
+    c_kv = torch.zeros(B, S, kv_lora_rank, device=device, dtype=dtype)
+    k_pe = torch.zeros(B, S, qk_rope_head_dim, device=device, dtype=dtype)
+    
+    q_nope[:, :S_ori, :, :] = q_nope_ori
+    q_pe[:, :S_ori, :, :] = q_pe_ori
+    c_kv[:, :S_ori, :] = c_kv_ori
+    k_pe[:, :S_ori, :] = k_pe_ori
+    
+    print("Computing Non absorb...")
+    q = torch.concat([q_nope, q_pe], dim=3)  # (B, S, h_q, qk_head_dim + qk_rope_head_dim)
+    c_kv_broadcast = c_kv.unsqueeze(-2).expand(-1, -1, h_q, -1).unsqueeze(-2)  # (B, S, h_q, 1, kv_lora_rank)
+    k_nope = (c_kv_broadcast @ wkv_b_q).squeeze(-2)  # (B, S, h_q, qk_head_dim)
+    
+    k = torch.concat([k_nope, k_pe.unsqueeze(-2).expand(-1, -1, h_q, -1)], dim=3)  # (B, S, h_q, qk_head_dim + qk_rope_head_dim)
+    v = (c_kv_broadcast @ wkv_b_o).squeeze(-2)  # (B, S, h_q, qk_head_dim)
+    
+    print(f'{q.shape=} {k.shape=} {v.shape=}')
+    out_ref, lse_ref = attention_causal_with_lse(
+        q.transpose(2, 1),
+        k.transpose(2, 1),
+        v.transpose(2, 1), scale=scale, causal=True, use_log2=use_log2)
+    out_ref = out_ref.transpose(2, 1)  # (B, S_ori, H, d)
+    print(f'{out_ref.shape=} {lse_ref.shape=}')
+
+    print("Computing absorb...")
+    q_absorb = (q_nope.unsqueeze(-2) @ wkv_b_q.transpose(-1, -2)).squeeze(-2)  # (B, S, h_q, kv_lora_rank)
+    q2 = torch.concat([q_absorb, q_pe], dim=3)  # (B, S, h_q, kv_lora_rank + qk_rope_head)
+    
+    k2_concat = torch.concat([c_kv, k_pe], dim=-1) # (B, S, 576)
+    k2 = k2_concat.unsqueeze(-2).expand(-1, -1, h_q, -1).clone()  # (B, S, h_q, kv_lora_rank + qk_rope_head)
+    v2 = c_kv.unsqueeze(-2).expand(-1, -1, h_q, -1).clone()  # (B, S, h_q, kv_lora_rank)
+    
+    print(f'{q2.shape=} {k2.shape=} {v2.shape=}')
+    out_ref2_raw, lse_ref2 = attention_causal_with_lse(
+        q2.transpose(2, 1),
+        k2.transpose(2, 1),
+        v2.transpose(2, 1), scale=scale, causal=True, use_log2=use_log2)
+    out_ref2_raw = out_ref2_raw.transpose(2, 1)
+    out_ref2 = (out_ref2_raw.unsqueeze(-2) @ wkv_b_o).squeeze(-2)
+    print(f'{out_ref2.shape=} {lse_ref2.shape=}')
+    
+    # compare
+    cmp(out_ref2, out_ref, "Torch Absorb vs Non-Absorb")
+    
+    print("Computing interleaved ring attention...")
+    q_dist = q2.reshape(B, S_local, cp_nranks, h_q, kv_lora_rank + qk_rope_head_dim)
+    # k_dist = k2.reshape(B, S_local, cp_nranks, h_q, kv_lora_rank + qk_rope_head_dim)
+    # v_dist = v2.reshape(B, S_local, cp_nranks, h_q, kv_lora_rank)
+    
+    c_kv_dist = c_kv.reshape(B, S_local, cp_nranks, kv_lora_rank)
+    k_pe_dist = k_pe.reshape(B, S_local, cp_nranks, qk_rope_head_dim)
+    
+    ring_final_out = torch.zeros((B, S_local, cp_nranks, h_q, kv_lora_rank), device=device, dtype=dtype)  # (B, S_local, CP, H, d)
+    for i in range(cp_nranks):
+        q_local = q_dist[:, :, i, :, :].transpose(1, 2) # (B, h_q, S_local, kv_lora_rank + qk_rope_head_dim)
+        
+        acc_out = None  # (B, H, S_local, d)  # float32
+        acc_lse = None  # (B, H, S_local)
+        for step in range(cp_nranks):
+            j = (i - step + cp_nranks) % cp_nranks
+            
+            c_kv_j = c_kv_dist[:, :, j, :] # (B, S_local, kv_lora_rank)
+            k_pe_j = k_pe_dist[:, :, j, :]  # (B, S_local, qk_rope_head_dim)
+            
+            k_concat_j = torch.concat([c_kv_j, k_pe_j], dim=-1)  # (B, S_local, kv_lora_rank + qk_rope_head_dim)
+            k_local = k_concat_j.unsqueeze(-2).expand(-1, -1, h_q, -1).transpose(1, 2)  # (B, h_q, S_local, kv_lora_rank + qk_rope_head)
+            v_local = c_kv_j.unsqueeze(-2).expand(-1, -1, h_q, -1).transpose(1, 2)  # (B, h_q, S_local, kv_lora_rank)
+            
+            if i >= j:
+                block_out, block_lse = attention_causal_with_lse(
+                    q_local, k_local, v_local, scale=scale, causal=True, use_log2=use_log2
+                )
+            else:
+                block_out_sub, block_lse_sub = attention_causal_with_lse(
+                    q_local[:, :, 1:, :],
+                    k_local[:, :, :-1, :], v_local[:, :, :-1, :],
+                    scale=scale, causal=True, use_log2=use_log2
+                )
+                
+                block_out = torch.zeros((B, h_q, S_local, kv_lora_rank), device=device, dtype=torch.float32)
+                block_lse = torch.zeros((B, h_q, S_local), device=device, dtype=torch.float32)
+                # trick: copy first
+                block_out[:, :, 0, :] = acc_out[:, :, 0, :]
+                block_lse[:, :, 0] = acc_lse[:, :, 0]
+                
+                block_out[:, :, 1:, :] = block_out_sub
+                block_lse[:, :, 1:] = block_lse_sub
+            
+            acc_out, acc_lse = update_out_and_lse(acc_out, acc_lse, block_out, block_lse, use_log2)
+        
+        ring_final_out[:, :, i, :, :] = acc_out.transpose(1, 2).to(dtype)
+
+    out_ring_raw = ring_final_out.view(B, S, h_q, kv_lora_rank).cuda()
+    out_ring = (out_ring_raw.unsqueeze(-2) @ wkv_b_o).squeeze(-2)
+    print(f'{out_ring.shape=}')
+    
+    print("\nFinal Comparison:")
+    cmp(out_ring[:, :S_ori, :, :], out_ref[:, :S_ori, :, :], "Interleaved Ring vs Torch Global")
+    
+    print(f'{torch.allclose(out_ring, out_ref, atol=1e-1, rtol=1e-6)=}')
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -25,43 +25,69 @@ def cmp(o1, o2, msg='cmp'):
     cos_sim = F.cosine_similarity(o1.flatten(), o2.flatten(), dim=0).item()
     
     print(f'{msg:35s}: CosSim={cos_sim:.6f}, MAE={mean_abs_error:.6f}, MaxError={max_error:.6f} ({value1:.6f} {value2:.6f})')
-    
+
 def attention_causal_with_lse(query_states, key_states, value_states,
-                              scale=None, causal=True, use_log2=False):
+                                  scale=None, causal=True, use_log2=False):
     """
-    计算单块内的因果注意力
-    Input Shape: (B, H, S, d)
-    Output: out 始终是全 base-e 的，lse 返回 lse/ln2 当 use_log2=True 时
+    计算单块内的因果注意力，支持 GQA (Grouped-Query Attention)。
+
+    - 当 Q, K, V 的头数量相同时，为标准的多头注意力 (MHA)。
+    - 当 K, V 只有 1 个头时，为多查询注意力 (MQA)。
+    - 当 Q 的头数量是 K, V 的整数倍时，为分组查询注意力 (GQA)。
+
+    Input Shapes:
+    - query_states: (B, H_q, S, d)  (B: batch_size, H_q: num_query_heads, S: seq_len, d: head_dim)
+    - key_states:   (B, H_k, S, d)  (H_k: num_key_value_heads)
+    - value_states: (B, H_k, S, d)
+
+    Output:
+    - out: 注意力输出，形状为 (B, H_q, S, d)，始终是全 base-e 的。
+    - lse: Log-Sum-Exp，形状为 (B, H_q, S)。当 use_log2=True 时返回 lse/ln2。
     """
-    B, H, S, d = query_states.shape
+    B, H_q, S, d = query_states.shape
+    _, H_k, _, _ = key_states.shape
+
+    # 如果是 GQA 或 MQA，需要重复 K 和 V 的头来匹配 Q
+    if H_q != H_k:
+        # 确认 H_q 是 H_k 的整数倍
+        if H_q % H_k != 0:
+            raise ValueError(f"GQA requires num_query_heads ({H_q}) to be a multiple of num_key_value_heads ({H_k})")
+        
+        num_reps = H_q // H_k
+        print(f'{num_reps=} {key_states.shape=} {query_states.shape=}')
+        # 沿头的维度重复 K 和 V
+        # (B, H_k, S, d) -> (B, H_k, 1, S, d) -> (B, H_k, num_reps, S, d) -> (B, H_q, S, d)
+        key_states = key_states.unsqueeze(2).expand(-1, -1, num_reps, -1, -1).reshape(B, H_q, S, d)
+        value_states = value_states.unsqueeze(2).expand(-1, -1, num_reps, -1, -1).reshape(B, H_q, S, d)
+        # 更高效的写法是使用 repeat_interleave
+        # key_states = key_states.repeat_interleave(num_reps, dim=1)
+        # value_states = value_states.repeat_interleave(num_reps, dim=1)
+
     if scale is None:
         scale = d ** -0.5
     
-    attn_scores = query_states @ key_states.transpose(-2, -1)  # (B, H, S, S)
+    # 经过 GQA 处理后，Q, K, V 的头数量都为 H_q，后续计算与 MHA 相同
+    attn_scores = query_states @ key_states.transpose(-2, -1)  # (B, H_q, S, S)
     attn_scores = attn_scores.float()
     attn_scores = attn_scores * scale
-
+    
     if causal:
         mask = torch.triu(torch.ones(S, S, device=query_states.device, dtype=torch.bool), diagonal=1)
         attn_scores.masked_fill_(mask, float('-inf'))
-
+        
     if not use_log2:
         # 1. 标准 LSE (base e)
-        lse = torch.logsumexp(attn_scores, dim=-1)  # (B, H, S)
+        lse = torch.logsumexp(attn_scores, dim=-1)  # (B, H_q, S)
     else:
         # 2. Base-2 LSE
         ln_2 = math.log(2.0)
-        # log2(sum(2^x)) = logsumexp(x * ln(2)) / ln(2)
-        # lse = torch.logsumexp(attn_scores * ln_2, dim=-1) / ln_2
-        # attn_weights = (attn_scores * ln_2).softmax(dim=-1)  # log2 时, out 也要使用基于 2 的 softmax 加权
-        
-        # 参考 FlashMLA，实际计算的是 log2(sum(e^x))
+        # 参考 FlashAttention/Flash-MLA，实际计算的是 log2(sum(e^x))
         lse = torch.logsumexp(attn_scores, dim=-1) / ln_2
     
     attn_weights = attn_scores.softmax(dim=-1)
     attn_weights = attn_weights.to(query_states.dtype)
-    out = attn_weights @ value_states  # (B, H, S, d)
-
+    out = attn_weights @ value_states  # (B, H_q, S, d)
+    
     return out, lse
 
 def update_out_and_lse(
